481819a0212c9dc26ae612352a1eeed75bf37577
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-08-25 07:31:31 +0100

    Initial Commits

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './App.css'
+import Form from './components/Form'
+
+function App() {
+  return (
+    <main className='flex items-center justify-center min-h-screen bg-gray-200'>
+      <Form />
+    </main>
+  )
+}
+
+export default App
